74508f2fb036f21b1f0b0da54ea4c20177f47408
Yoyo Peng <yoyopyeah@gmail.com>
2022-01-22 12:51:10 -0500

    update

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+
+
 const app = Vue.createApp({
     data() {
       return {
-        course: 'COMP 303',
-        description: 'the asgn is so hard lol let\'s work on them',
-        time: 'Jan. 24',
-        location: 'trottier',
-        capacity: 10,
-        joined: 5
+          groups: [
+              {
+                course: 'COMP 303',
+                description: 'the asgn is so hard lol let\'s work on them',
+                time: 'Jan. 24',
+                location: 'trottier',
+                capacity: 10,
+                joined: 5
+              },
+              {
+                course: 'MATH 222',
+                description: 'omg kelome',
+                time: 'Jan. 25',
+                location: 'Burnside',
+                capacity: 8,
+                joined: 3
+              }
+          ]
+        
       }
     },
     methods: {
       newJoin() {
           if (this.joined + 1 <= this.capacity) {
-              this.joined++;
+              this.joined++
+          } else {
+            window.alert("Sorry! Study group capacity reached!")
           }
       }
     },
